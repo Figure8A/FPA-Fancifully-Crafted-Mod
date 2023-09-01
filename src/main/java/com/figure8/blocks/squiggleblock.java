35c1bf2cbf2872b1cb3b8d6cae72ee76a60d49eb
Figure8A <solomoncastro080@gmail.com>
2023-09-01 02:15:27 -0700

    many freaking things AUGGHHHH

--- src/main/java/com/figure8/blocks/squiggleblock.java
+++ src/main/java/com/figure8/blocks/squiggleblock.java
@@ -1,35 +1,53 @@
 package com.figure8.blocks;
 
+import com.figure8.sound.ModSounds;
 import net.minecraft.block.*;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
-
-
+import net.minecraft.world.World;
 
 
 public class squiggleblock
-        extends Block {
+        extends HorizontalFacingBlock {
 
-    protected static final VoxelShape BOUNDING_SHAPE = Block.createCuboidShape(4, 4, 3, 12, 13, 11);
+    protected static final VoxelShape BOUNDING_SHAPE = Block.createCuboidShape(4, 2, 4, 12, 13, 12);
     public squiggleblock(AbstractBlock.Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH));
     }
 
-    @Override
-    public boolean isTransparent(BlockState state, BlockView world, BlockPos pos) {
-        return true;
-    }
+
 
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return BOUNDING_SHAPE;
     }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.HORIZONTAL_FACING);
+    }
+
 
     @Override
-    public float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
-        return 1.0f;
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing().getOpposite());
     }
+
 
 }
 
